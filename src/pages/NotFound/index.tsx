1aa0d618aae4c282b88aad8506455eb1348734f2
KIMSEI1124 <workju1124@gmail.com>
2023-11-17 13:55:37 +0900

    Feat(#3): 404 페이지 기능 구현

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,5 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+
+import NotFoundAnimation from '@/components/Animation/NotFound';
+import { BROWSER_PATH } from '@/constants/path';
+
+import * as S from './index.styled';
+
 function NotFound() {
-  return <div>Not Found</div>;
+  const navigate = useNavigate();
+
+  const handleClickGoHome = () => {
+    navigate(BROWSER_PATH.BASE);
+  };
+
+  const handleClickGoBack = () => {
+    history.back();
+  };
+
+  return (
+    <S.Container>
+      <NotFoundAnimation />
+      <S.Wrapper>
+        <S.Button onClick={handleClickGoHome}>Go Home</S.Button>
+        <S.Button onClick={handleClickGoBack}>Go Back</S.Button>
+      </S.Wrapper>
+    </S.Container>
+  );
 }
 
 export default NotFound;
