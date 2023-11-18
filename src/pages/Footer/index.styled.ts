158ef0a8630d422cb972935f55d094a3d62f4093
KIMSEI1124 <workju1124@gmail.com>
2023-11-18 15:18:32 +0900

    Design(#9): Footer 디자인

---
+++ src/pages/Footer/index.styled.ts
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 125px;
+
+  border-top: 1px solid #33333350;
+`;
+
+const Comment = styled.p`
+  font-size: 1.1rem;
+  color: gray;
+`;
+
+export { Container, Comment };
---
+++ src/pages/Header/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './index.styled';
+
+function Header() {
+  return (
+    <S.Container>
+      <div>2</div>
+    </S.Container>
+  );
+}
+
+export default Header;
