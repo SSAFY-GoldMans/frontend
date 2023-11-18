31b6dfb2a72211274ada6014e750cd15a5441237
KIMSEI1124 <workju1124@gmail.com>
2023-11-18 14:45:22 +0900

    Refactor(#7): 검색 옵션 상수로 수정

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,3 +1,4 @@
+import { TimeOption } from '@/constants/filter';
 import SearchMapAnimation from '@/components/Animation/SearchMap';
 import SearchInput from '@/components/SearchInput';
 import SelectBox from '@/components/SelectBox';
@@ -5,20 +6,13 @@ import SelectBox from '@/components/SelectBox';
 import * as S from './index.styled';
 
 function Landing() {
-  const SelectTimes: string[] = [
-    '상관 없음',
-    '15분 이내',
-    '30분 이내',
-    '60분 이내',
-  ];
-
   return (
     <S.Container>
       <SearchMapAnimation />
       <SearchInput />
       <S.OptionWrapper>
         <S.Comment>이동 시간</S.Comment>
-        <SelectBox option={SelectTimes} />
+        <SelectBox option={TimeOption} />
       </S.OptionWrapper>
     </S.Container>
   );
